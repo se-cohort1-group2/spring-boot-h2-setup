10ef699a54f466bf0d05da77137749e03fb82bb2
xong002 <xong002@gmail.com>
2023-03-17 20:21:08 +0800

    add response status to ticket /GET

--- m3project/src/main/java/sg/edu/ntu/m3project/m3project/controller/TicketController.java
+++ m3project/src/main/java/sg/edu/ntu/m3project/m3project/controller/TicketController.java
@@ -3,7 +3,9 @@ package sg.edu.ntu.m3project.m3project.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +20,13 @@ public class TicketController {
     @Autowired
     TicketRepository ticketRepo;
 
-    @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<TicketEntity>> findAll() {
+    @RequestMapping(method = RequestMethod.GET) // to include userId in RequestHeader
+    public ResponseEntity<?> findAll() {
         List<TicketEntity> tickets = (List<TicketEntity>) ticketRepo.findAll();
-        return ResponseEntity.ok().body(tickets);
+        if (tickets.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No ticket history");
+        } else
+            return ResponseEntity.ok().body(tickets);
     }
 
 }
