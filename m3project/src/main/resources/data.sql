1443b11ef90db94ebaebe11ccf340925ef5bde01
yxlye <lye.yong.xin.9660@gmail.com>
2023-03-17 20:57:34 +0800

    updated concerts

--- m3project/src/main/resources/data.sql
+++ m3project/src/main/resources/data.sql
@@ -1,1 +1,4 @@
 insert into users (email) values ('admin@mail.com');
+insert into concerts (artist, concert_date, tickets_available, ticket_price) values 
+('May Day', '2023-05-05 19:30:00', 100, 188),
+('Blackpink', '2023-05-13 19:30:00', 120, 388);
--- m3project/src/main/resources/schema.sql
+++ m3project/src/main/resources/schema.sql
@@ -4,3 +4,13 @@ create table public.users (
     created_at timestamp default current_timestamp,
     primary key (id)
 );
+
+create table public.concerts (
+    id int auto_increment,
+    artist varchar(255),
+    concert_date datetime,  
+    tickets_available int,
+    ticket_price float, 
+    created_at timestamp default current_timestamp,
+    primary key (id)
+);
